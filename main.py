1455e81012020040550fd1c8398cd2b80ae56aec
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-03-21 00:53:06 +0530

    update

--- main.py
+++ main.py
@@ -4,3 +4,25 @@ import openai
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+openai.api_key=os.getenv("OPENAI_API_KEY")
+
+def chat_with_gpt(prompt):
+    client=openai.OpenAI()
+    response=client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role":"system","content":"You are a helpful assistant."},
+                  {"role":"user","content":prompt}]
+
+ )
+    return response['choices'][0]['message']['content']
+
+print("Kedzil:Hello!, Type 'exit' to end the conversation.")
+
+while True:
+    user_input=input("You:")
+    if user_input.lower()==exit:
+        print("Kedzil:Radhe Radhe :)")
+        break
+    response=chat_with_gpt(user_input)
+    print("Kedzil:",response)
